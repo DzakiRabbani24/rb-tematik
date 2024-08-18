270ca8693961694d59e224ab43e69c224ea398c2
DzakiRabbani24 <dzakrob24.14@gmail.com>
2024-08-18 23:07:04 +0700

    update tampilan table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,8 @@
 <body>
     <div id="app">
         @include('layouts.navbar')
-
+        
+        @yield('content1')
         <div class="container mt-4">
             @yield('content')
         </div>
